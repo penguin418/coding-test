51c1fc772fc4ff0c0be7e7668f7b49af2e8a052b
penguin418 <penguin418@users.noreply.github.com>
2020-09-24 22:33:46 +0900

    [터렛] 두 원의 접점의 개수
    점점개수는 d1 < r1-r2 < d2 < r1+r2 < d3 일때 달라짐
    d1: 0개, d2: 2개, d3: 0개, d4=r1+r2일때, 1개

---
+++ acmicpc_1003.cpp
@@ -0,0 +1,28 @@
+#include<cstdio>
+
+int main(){
+    int n;
+    int x1, y1, r1, x2, y2, r2;
+    scanf("%d", &n);
+    for (int i=0; i<n; i++){
+        scanf("%d %d %d %d %d %d", &x1, &y1, &r1, &x2, &y2, &r2);
+        int d = (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2);
+        int rDiff = (r1-r2)*(r1-r2);
+        int rSum = (r1+r2)*(r1+r2);
+        if(d == 0){
+        	if (rDiff == 0){
+        		printf("-1\n");
+        	}else{
+        		printf("0\n");
+        	}
+        }else{
+ 
+        if ((rDiff < d) && (d < rSum))
+            printf("2\n");
+        else if ((rDiff == d) || (rSum == d))
+            printf("1\n");
+        else
+            printf("0\n");
+        }
+    }
+}
